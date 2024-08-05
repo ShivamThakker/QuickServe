18049e347c8dcefd06ae1a1c6c0dcf6efcc4debd
Adit Shah <shah.adit1@northeastern.edu>
2024-08-05 18:47:28 -0400

    Add files via upload

---
+++ Frontend/src/components/MapComponent.js
@@ -0,0 +1,39 @@
+// components/MapComponent.js
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import './MapComponent.css';
+
+// Define custom icon for the markers
+const customIcon = new L.Icon({
+  iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-red.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+const MapComponent = ({ cities }) => {
+  return (
+    <MapContainer center={[44.0, -71.0]} zoom={6} className="map-container">
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {Object.keys(cities).map(state => (
+        cities[state].map(city => (
+          <Marker
+            key={city}
+            position={[city.lat, city.lng]} // Ensure cities have lat and lng properties
+            icon={customIcon}
+          >
+            <Popup>{city.name}</Popup>
+          </Marker>
+        ))
+      ))}
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
